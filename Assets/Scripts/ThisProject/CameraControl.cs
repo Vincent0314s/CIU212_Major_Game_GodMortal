b9d3a465ea5171079b6d44436461c9dd1065453a
Vincent <3d314831994@gmail.com>
2021-03-21 16:05:50 +1100

    Camera Following

---
+++ Assets/Scripts/ThisProject/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    [Range(0,1)]
+    public float followSpeed;
+
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Slerp(transform.position,target.position + offset, followSpeed);
+    }
+
+}
