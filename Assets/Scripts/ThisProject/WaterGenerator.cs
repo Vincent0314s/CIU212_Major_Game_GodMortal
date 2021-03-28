11da6ec379255c6022692f3f5b1d1ca78ebc04af
Vincent <3d314831994@gmail.com>
2021-03-28 16:41:55 +1100

    Imported WaterNoise scripts for lava

---
+++ Assets/Scripts/ThisProject/WaterGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(WaterNoise))]
+public class WaterGenerator : MonoBehaviour
+{
+    public float size = 0.85f;
+    public int gridSize = 16;
+
+    private MeshFilter filter;
+
+    void Start()
+    {
+        GenerateWaterMesh();
+    }
+
+    public void GenerateWaterMesh()
+    {
+        filter = GetComponent<MeshFilter>();
+        filter.mesh = GenerateMesh();
+    }
+
+    private Mesh GenerateMesh()
+    {
+
+        Mesh m = new Mesh();
+
+        List<Vector3> verticies = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for (int x = 0; x < gridSize + 1; x++)
+        {
+            for (int y = 0; y < gridSize + 1; y++)
+            {
+                verticies.Add(new Vector3(-size * 0.5f + size * (x / ((float)gridSize)), 0, -size * 0.5f + size * (y / ((float)gridSize))));
+                normals.Add(Vector3.up);
+                uvs.Add(new Vector2(x / (float)gridSize, y / (float)gridSize));
+            }
+        }
+
+        var triangles = new List<int>();
+        var vertCount = gridSize + 1;
+        for (int i = 0; i < vertCount * vertCount - vertCount; i++)
+        {
+            if ((i + 1) % vertCount == 0)
+            {
+                continue;
+            }
+            triangles.AddRange(new List<int>() {
+                i+1+vertCount, i+vertCount, i, i, i+1, i+vertCount+1
+            });
+
+        }
+
+        m.SetVertices(verticies);
+        m.SetNormals(normals);
+        m.SetUVs(0, uvs);
+        m.SetTriangles(triangles, 0);
+
+        return m;
+    }
+}
